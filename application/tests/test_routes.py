35db851f515c7e5d9ef6e0c8c24d6a602493a176
Jonathan <jonathan_lintott@outlook.com>
2018-08-12 17:29:31 +0100

    Add skeleton of tests

---
+++ application/tests/test_routes.py
@@ -0,0 +1,28 @@
+from application.tests.base import BaseTestCase
+
+
+class TestShortenUrl(BaseTestCase):
+    def test_no_body_data(self):
+        pass
+
+    def test_no_url_in_data(self):
+        pass
+
+    def test_invalid_url(self):
+        pass
+
+    def test_valid_url(self):
+        pass
+
+
+class TestRetrieveURL(BaseTestCase):
+    def test_no_record(self):
+        pass
+
+    def test_successful_redirect(self):
+        pass
+
+
+class TestIntegration(BaseTestCase):
+    def test_store_and_retrieve(self):
+        pass
