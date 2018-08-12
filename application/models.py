bcf27335dd64e80384c418a8acb84de569dffbf3
Jonathan <jonathan_lintott@outlook.com>
2018-08-12 19:27:01 +0100

    Add database migration

--- application/models.py
+++ application/models.py
@@ -6,6 +6,7 @@ from application import db
 class UrlPair(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     original_url = db.Column(db.Text(), nullable=False)
-    shortened_url = db.Column(db.String(32), nullable=False)
+    shortened_url = db.Column(db.String(32), nullable=False,
+                              index=True)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow,
                           nullable=False)
